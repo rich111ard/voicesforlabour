4de323cceac7b41caa6aa646dc2fce23ab07fc94
User <user@Users-MacBook-Pro.local>
2026-03-19 20:49:22 +0300

    Updates

---
+++ assets/index-RwZzBBvx.js
@@ -0,0 +1,1 @@
+import{f as r}from"./index-Cx8OCIt4.js";var a=r();export{a as r};
---
+++ assets/send-Dji5ujd3.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-Cx8OCIt4.js";/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719",key:"1sd12s"}]],o=a("message-circle",e);/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],t=a("send",c);export{o as M,t as S};
